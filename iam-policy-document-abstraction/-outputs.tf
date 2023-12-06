525aec3a0448133a6b1fc22e358eb77ff4121877
Jheison Rodriguez <jheison.rodriguez@stratusgrid.com>
2023-12-06 11:56:24 -0500

    Upgrade TF version

--- iam-policy-document-abstraction/-outputs.tf
+++ iam-policy-document-abstraction/-outputs.tf
@@ -1,4 +1,4 @@
 output "iam_policy_json" {
   description = "Outputs the iam policy in json format"
-  value       = join(",", concat(data.aws_iam_policy_document.assume_role_policy.*.json, data.aws_iam_policy_document.assume_role_policy_mfa.*.json))
+  value       = join(",", concat(data.aws_iam_policy_document.assume_role_policy[*].json, data.aws_iam_policy_document.assume_role_policy_mfa[*].json))
 }
--- tags.tf
+++ tags.tf
@@ -1,5 +1,6 @@
 locals {
-  #tflint-ignore: terraform_unused_declarations -- Ignoring warning of unused declaration as this declaration will be called during module instantiation
+  #Ignoring warning of unused declaration as this declaration will be called during module instantiation
+  #tflint-ignore: terraform_unused_declarations
   common_tags = merge(var.input_tags, {
     "ModuleSourceRepo" = "github.com/StratusGrid/terraform-aws-iam-cross-account-trust-maps"
   })
